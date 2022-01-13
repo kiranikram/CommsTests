0235a0884f4cae8dda447525a48363cdf536686d
kiranikram <kiran.ikram91@gmail.com>
2022-01-13 18:46:27 +0000

    basic env done

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -11,7 +11,7 @@ from src.env.basic_env import SeasonForecast
 
 
 
-
+#assert season is same for both agents at same time 
 def test_rl_loop():
     """
     Tests the general RL api.
@@ -20,12 +20,18 @@ def test_rl_loop():
     agent_1 = Agent()
     agent_2 = Agent()
 
-    basic_env = SeasonForecast(periods=12,n_steps=24)
+    basic_env = SeasonForecast(periods=12,n_steps=24,occlusion_pct=0.4)
 
     basic_env.generate_weather
-    print(basic_env.forecast)
+
+ 
+    action_ag_1 = agent_1.get_random_actions
+    action_ag_2 = agent_2.get_random_actions
+
     
-    basic_env.add_agents(agent_1, agent_2)
+
+    agent_obs = basic_env.generate_agent_obs
+    
 
     # make some assertions that 
 
