810de190e186b9b4ca1db02b6a6d2905927ac4e7
kiranikram <kiran.ikram91@gmail.com>
2022-01-17 12:02:08 +0000

    commenced comms

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -24,16 +24,29 @@ def test_rl_loop():
 
     basic_env.generate_weather
 
- 
-    action_ag_1 = agent_1.get_random_actions
-    action_ag_2 = agent_2.get_random_actions
+    agent_1.get_random_actions
+    agent_2.get_random_actions
+
+def test_agent():
+    agent_1 = Agent()
+    agent_2 = Agent()
+    basic_env = SeasonForecast(periods=12,n_steps=24,occlusion_pct=0.4)
+
+    basic_env.generate_weather
+
+    actions = {
+        agent_1: {
+            agent_1.get_random_actions()
+        },
+        agent_2: {
+            agent_2.get_random_actions()
+        }
+    }
+
+    print(actions)
+
+    basic_env.step(actions)
 
     
-
-    agent_obs = basic_env.generate_agent_obs
-    
-
-    # make some assertions that 
-
   
     
